cb69eefc134ff0d1cfc0a4614889acef05a7cba7
Yuu Miino <ymiino@naruto-u.ac.jp>
2023-03-10 16:28:36 +0900

    Write: 1.1 OK

--- note/contraction.py
+++ note/contraction.py
@@ -16,3 +16,24 @@ atop = Hg @ Jf
 # Comparison
 print(cont24 == atop) # (2, 3, 4) tensor in X, W*, V*, with All True
 print(cont34 == atop) # (2, 3, 4) tensor in X, W*, V*, with All True
+
+# (2, 4) contraction (for W*, W)
+cont24 = np.trace(np.tensordot(Hg @ Jf, Jf, axes=0), axis1=1, axis2=3)
+
+# @ operation
+atop = (Hg @ Jf).transpose(0, 2, 1) @ Jf
+
+print(cont24 == atop) # (2, 4, 4) tensor in X, V*, V*, with All True
+
+Jg = np.random.randint(100, size=6).reshape(2, 3)       # X, W*
+Hf = np.random.randint(100, size=48).reshape(3, 4, 4)   # W, V*, V*
+Hf = (Hf + Hf.transpose(0, 2, 1)) / 2                   # Make Hf symmetric
+
+# (2, 3) contraction (for W*, W)
+cont23 = np.trace(np.tensordot(Jg, Hf, axes=0), axis1=1, axis2=2)
+
+# @ operation
+atop = Jg @ Hf.transpose(1, 0, 2)   # (4, 2, 4) tensor
+atop = atop.transpose(1, 0, 2)      # (2, 4, 4) tensor
+
+print(cont23 == atop) # (2, 4, 4) tensor in X, V*, V*, with All True
