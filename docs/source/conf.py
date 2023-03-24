9644611da93c9ccfb2e86f3389a6288b06c11ba1
Yuu-Miino <miino.yuu@gmail.com>
2023-03-24 23:14:41 +0900

    Version dropdown OK

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,48 +14,6 @@ project = 'MapPy'
 copyright = '2023, Yuu Miino'
 author = 'Yuu Miino'
 
-def get_version_number(st):
-    import re
-
-    """
-    A function that extracts the version number from a string that is enclosed in single or double quotes.
-    by ChatGPT
-
-    Parameters
-    ----------
-    st : str
-        The input string containing the version number enclosed in quotes.
-
-    Returns
-    -------
-    str or None
-        The extracted version number as a string. If no version number is found, returns None.
-
-    Examples
-    --------
-    >>> get_version_number("version = '0.0.4'")
-    '0.0.4'
-    >>> get_version_number('version = "0.0.5"')
-    '0.0.5'
-    """
-    pattern = r"(?<=['\"])[^'\"]+(?=['\"])"  # Regular expression pattern
-    match = re.search(pattern, st)  # Get the match for the regular expression in the string
-    if match is None:
-        return None  # Return None if no match is found
-    return match.group()  # Return the matched substring
-
-version_file = '../../mappy/_version.py'
-if os.path.exists(version_file):
-    with open(version_file) as f:
-        version = get_version_number(f.read())
-        if version is None:
-            version = '0.0.1'
-else:
-    version = '0.0.1'
-release = version
-with open('out.dat', 'a') as f:
-    f.write('1 '+version+str(type(version))+'\n')
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -63,7 +21,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'numpydoc',
     'sphinx.ext.autosummary',
-#    'sphinx_automodapi.automodapi',
     'sphinx.ext.mathjax',
     'sphinx_design',
     'sphinx_multiversion',
@@ -97,10 +54,10 @@ html_theme_options = {
         "image_dark": "https://miino.sytes.net/figs/089-traj.gif",
     },
     "switcher": {
-        "version_match": "current_version.name",
+        "version_match": "0.0.0",
         "json_url": "https://yuu-miino.github.io/MapPy/master/_static/switcher.json",
     },
-    "navbar_start": ["navbar-logo", "version-switcher"]
+    "navbar_start": ["navbar-logo", "version.html"]
 }
 
 html_list = ["search-field.html", "sidebar-nav-bs.html"]
