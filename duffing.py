7c252b5ea62d503a504277d411f2f020fe47641b
Yuu Miino <ymiino@naruto-u.ac.jp>
2023-10-10 14:26:19 +0900

    Update duffing.py

--- duffing.py
+++ duffing.py
@@ -57,44 +57,79 @@ def main():
         param_idx=0,
     )
 
-    run_mode = "fix_trace"
+    run_mode = "single_bf"
 
-    if run_mode == "traj":
-        mplot2d(pmap, y0, "p", param, config=config)
-    elif run_mode == "fix_trace":
-        ret = trace_cycle(
-            diff=pmap,
-            y0=y0,
-            m0="p",
-            params=param,
-            period=1,
-            cnt_param_key="B",
-            end_val=0.375,
-            show_progress=True,
-            resolution=50,
-        )
-        pxy = np.array(
-            [[r[1]["B0"], r[1]["B"], *r[0], *r[2]["eigvals"]] for r in ret],
-            dtype=np.floating,
-        )
-        np.savetxt("duffing_fix_trace.csv", pxy, delimiter=",")
-    elif run_mode == "bf_Trace":
-        ret_lbf = trace_local_bf(
-            diff=pmap,
-            y0=y0,
-            m0="p",
-            params=param,
-            bf_param_key="B0",
-            theta=np.pi,
-            cnt_param_key="B",
-            end_val=0.35,
-            period=1,
-            resolution=100,
-            show_progress=True,
-        )
-        print(len(ret_lbf))
-    else:
-        pass
+    match run_mode:
+        case "traj":
+            mplot2d(pmap, y0, "p", param, config=config)
+        case "fix_trace":
+            ret = trace_cycle(
+                diff=pmap,
+                y0=y0,
+                m0="p",
+                params=param,
+                period=1,
+                cnt_param_key="B",
+                end_val=0.375,
+                show_progress=True,
+                resolution=50,
+            )
+            pxye = np.array(
+                [[r[1]["B0"], r[1]["B"], *r[0], *r[2]["eigvals"]] for r in ret],
+                dtype=np.floating,
+            )
+            np.savetxt("duffing_fix_trace.csv", pxye, delimiter=",")
+        case "bf_Trace":
+            ret_lbf = trace_local_bf(
+                diff=pmap,
+                y0=y0,
+                m0="p",
+                params=param,
+                bf_param_key="B0",
+                theta=np.pi,
+                cnt_param_key="B",
+                end_val=0.35,
+                period=1,
+                resolution=100,
+                show_progress=True,
+            )
+            print(len(ret_lbf))
+        case "1dim_bf":
+            resolution = 1000
+            Brange = np.linspace(0.1, 0.375, resolution)
+            maps = 100
+            with open("duffing_1dim_bf.csv", "w") as f:
+                c = 0
+                for B in Brange:
+                    param["B"] = B
+                    for i in range(maps):
+                        y0 = pmap.image(y0=y0, m0="p", params=param)
+                        f.write(f"{param['B0']},{param['B']},{y0[0]},{y0[1]}\n")
+                        print(
+                            "\t",
+                            f"[{c:04d}/{resolution:04d} | {i:03d}]",
+                            "B0:",
+                            param["B0"],
+                            "B:",
+                            param["B"],
+                            "y0:",
+                            y0,
+                            end="\r",
+                        )
+                    f.write("\n")
+                    print()
+                    c += 1
+
+        case "single_bf":
+            y0 = np.array([-3.115916368789392599e-01, 1.422745846218321231e-01])
+            param["B"] = 3.6938e-01
+
+            ret = find_local_bf(
+                pmap, y0, m0="p", params=param, param_key="B", theta=np.pi
+            )
+            print(ret)
+        case _:
+            pass
 
 
 if __name__ == "__main__":
